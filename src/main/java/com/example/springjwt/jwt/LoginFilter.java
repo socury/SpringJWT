2fb4f5d478c1351ada024b6ce0929e1819247166
이기찬 <socury1104@gmail.com>
2024-09-05 08:40:12 +0900

    DB-based login validation logic

--- src/main/java/com/example/springjwt/jwt/LoginFilter.java
+++ src/main/java/com/example/springjwt/jwt/LoginFilter.java
@@ -1,19 +1,22 @@
 package com.example.springjwt.jwt;
 
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
-import javax.naming.AuthenticationException;
 
+@Slf4j
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
     public LoginFilter(AuthenticationManager authenticationManager) {
-
         this.authenticationManager = authenticationManager;
     }
 
@@ -23,6 +26,23 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+
+        log.info(username);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authToken);
+    }
+
+    //로그인 성공
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    //로그인 실패
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
 
     }
 }
