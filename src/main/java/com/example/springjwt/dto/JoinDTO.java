e7c143ed28272620b0a19e727364a59b99737cf8
이기찬 <socury1104@gmail.com>
2024-09-14 23:43:41 +0900

    JWT validation logic

--- src/main/java/com/example/springjwt/dto/JoinDTO.java
+++ src/main/java/com/example/springjwt/dto/JoinDTO.java
@@ -1,13 +1,16 @@
 package com.example.springjwt.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class JoinDTO {
+    @NotNull
     private String username;
 
+    @NotNull
     private String password;
 
 }
